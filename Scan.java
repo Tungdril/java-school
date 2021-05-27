8935d5a9173dc0ce4bb80bd080356aa0e4a2ae7b
Tungdril <laurin.soerensen@gmx.de>
2021-05-27 08:44:53 +0200

    scan

---
+++ Scan.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Scan{
+
+
+    public static void main(String[] args){
+        System.out.println("Bitte geben sie Ihren namen an: ");
+        Scanner nameScanner = new Scanner(System.in);
+        String name = nameScanner.next();
+        nameScanner.close();
+
+        System.out.println("Bitte geben sie Ihren Kontostand an: ");
+        Scanner walletScanner = new Scanner(System.in);
+        double wallet = walletScanner.nextDouble();
+        walletScanner.close();
+
+        if(wallet>0.0){
+            System.out.printf(name+" guten Tag! Viel spaß mit Ihren ");
+            System.out.printf("%.2f", wallet);
+            System.out.printf(" Euro.\n");
+        }else{
+            System.out.printf(name+", bezahlen Sie ");
+            System.out.printf("%.2f", (Math.abs(wallet)));
+            System.out.printf(" Euro.\n");
+        }
+    }
+
+}
