75df11fa164ff6edf98f6e937cade8d124601a0a
Laurin Sörensen <49350541+Tungdril@users.noreply.github.com>
2021-06-09 11:05:44 +0200

    Update Password.java

--- Password.java
+++ Password.java
@@ -4,12 +4,11 @@ public class Password{
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-
         String bn, pwd;
        
         for(int i = 3; i != 0; i--){
 
-        System.out.println("Noch "+ i + " versuche\n");    
+        System.out.println("Sie können es noch "+ i + " mal versuchen\n");    
 
         System.out.println("Bitte Benutzernahmen eingeben: ");
         bn = input.nextLine();
@@ -22,10 +21,10 @@ public class Password{
             input.close();
             System.exit(0);  
         } else {
-            System.out.println("Ihre Daten sind inkorrekt");
+            System.out.println("Ihre Daten sind inkorrekt\n");
             }   
         } 
-        System.out.println("Keine weiteren versuche möglich!"); 
+        System.out.println("Keine weiteren Versuche möglich! \n Bitte wenden Sie sich an einen Administrator."); 
         System.exit(0);     
     }
 }
